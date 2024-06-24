3887b6fcc6490d8041dfb685e58283449fe96b32
Itxaka <itxaka@kairos.io>
2024-06-24 14:02:28 +0200

    Test for SB signature
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- pkg/pesign/pesign_test.go
+++ pkg/pesign/pesign_test.go
@@ -1,6 +1,8 @@
 package pesign
 
 import (
+	"github.com/foxboron/go-uefi/authenticode"
+	"github.com/foxboron/go-uefi/pkcs7"
 	"os"
 	"path/filepath"
 	"testing"
@@ -34,8 +36,33 @@ var _ = Describe("Pesign tests", func() {
 	})
 	Describe("Signs correctly a PE file", func() {
 		It("Signs correctly a file", func() {
-			err := sbSigner.Sign("testdata/file.efi", filepath.Join(tmpDir, "file.signed.efi"))
+			// Check first that the base input has no signatures
+			f1, err := os.Open("testdata/file.efi")
 			Expect(err).ToNot(HaveOccurred())
+			binary, err := authenticode.Parse(f1)
+			defer f1.Close()
+			signatures, err := binary.Signatures()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(signatures).To(HaveLen(0))
+
+			// Now we can continue signing and checking if the final file is signed
+			err = sbSigner.Sign("testdata/file.efi", filepath.Join(tmpDir, "file.signed.efi"))
+			Expect(err).ToNot(HaveOccurred())
+			f2, err := os.Open(filepath.Join(tmpDir, "file.signed.efi"))
+			Expect(err).ToNot(HaveOccurred())
+			defer f2.Close()
+			binary, err = authenticode.Parse(f2)
+			Expect(err).ToNot(HaveOccurred())
+			signatures, err = binary.Signatures()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(signatures).To(HaveLen(1))
+			for _, signature := range signatures {
+				parsedPKCS7, err := pkcs7.ParsePKCS7(signature.Certificate)
+				Expect(err).ToNot(HaveOccurred())
+				// Expect the signatures in the signed file to match our key/cert issuer
+				Expect(parsedPKCS7.Certs[0].Issuer.CommonName).To(Equal("Kairos DB"))
+				Expect(parsedPKCS7.Certs[0].Subject.CommonName).To(Equal("Kairos DB"))
+			}
 
 		})
 
